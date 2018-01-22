1fbff2e2fb9f2c9afe3c92c7d1d2dd252ec5b915
Nathan <ndbarteaux@gmail.com>
2018-01-22 02:17:05 -0700

    header

---
+++ header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<meta name="viewport" content="width=device-width; initial-scale=1.0">
+		<style>
+		html {
+			background-color: black;
+		}
+		.header {
+			text-align: center;
+			border-style: dashed;
+			background-color: green;
+		}
+		.content {
+			background-color: black;
+			color: red;
+		}
+		.footer {
+			  position: absolute;
+  			right: 0;
+  			bottom: 0;
+  			left: 0;
+  			padding: 1rem;
+  			background-color: #efefef;
+  			text-align: center;
+		}
+		</style>
+		<title>CT 310 Assignemnt 7</title>
+		<meta name="description" content="">
+		        <meta name="keywords" content="">
+	</head>
+	<body>
+		<div class="header">
+			<h1><a href="https://www.cs.colostate.edu/~ct310/yr2017sp/more_assignments/assignment07.php">CT 310 			Assignment 7</a></h1>
+			<h3>Countdown Timer using Javascript</h3>
+		</div>
+		<div class="content">
